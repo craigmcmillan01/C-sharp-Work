8cd0a3359fd20f0675e8efe58f54241aa5b44b13
Craig McMillan <10004794@napier.ac.uk>
2012-10-09 22:55:30 +0100

    case study

---
+++ Software Engineering/CaseStudy_01/ClinicSystem_01/Appointment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClinicSystem_01
+{
+    public class Appointment
+    {
+        public int day { get; set; }
+        public int month { get; set; }
+        public int year { get; set; }
+       
+        public Appointment(int day, int month, int year)
+        {
+           this.day = day;
+           this.month = month;
+           this.year = year;
+        }
+        
+       
+    }
+}
---
+++ Software Engineering/CaseStudy_01/ClinicSystem_01/MedicalStaff.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClinicSystem_01
+{
+    public abstract class MedicalStaff
+    {
+        // Properties
+        protected virtual string firstName { get; set; }
+        protected string secondName { get; set; }
+        protected int staffNumber { get; set; }
+        List<Appointment> appointments = new List<Appointment>();
+
+        // Constructors
+        public MedicalStaff() { }
+        public MedicalStaff(string firstName, string secondName, int id)
+        {
+            this.firstName = firstName;
+            this.secondName = secondName;
+            this.staffNumber = id;
+        }
+
+        //methods
+        public void addAppointment(Appointment a)
+        {
+          this.appointments.Add(a);
+        }
+    }
+}
