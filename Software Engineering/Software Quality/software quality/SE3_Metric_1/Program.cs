ca3a2bd0ae203180220c14ba02a1908c3e33d0d3
Craig McMillan <10004794@napier.ac.uk>
2012-10-30 15:18:53 +0000

    coursework

---
+++ Software Engineering/Software Quality/software quality/SE3_Metric_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace SE3_Metric_1
+{
+    // Rooter:  Simple C# program to input an integer and output its square root
+    // Greg McCarra, 10/10/12
+    //
+    class Rooter
+    {
+        static void Main()
+        {
+            string s_input, s_output;
+            int val;
+            double res;
+            System.Console.WriteLine("Please enter a positive integer:  ");
+            s_input = System.Console.ReadLine();
+            val = int.Parse(s_input);
+            res = Math.Sqrt(val);
+            s_output = "The square root of " + val + " is " + res;
+            System.Console.WriteLine(s_output);
+            System.Console.Read();
+        }
+    }
+}
